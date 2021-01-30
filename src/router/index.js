8d71e26e827ce4b3e7433cf9ba08c120f9f0ec7d
tuchella <lars-tuchel@outlook.com>
2021-01-30 19:48:19 +0100

    Handle routing and add 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Explorer from '../views/Explorer.vue'
+import NotFound from '../views/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -8,8 +9,28 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
+    alias: '/lorenz',
     name: 'Explorer',
-    component: Explorer
+    component: Explorer,
+    props: {
+      color: "blue"
+    }
+  },
+  {
+    path: '/overflow',
+    name: 'OverflowExplorer',
+    component: Explorer,
+    props: {
+      color: "red"
+    }
+  },
+  {
+    path: '/uri',
+    name: 'UriExplorer',
+    component: Explorer,
+    props: {
+      color: "yellow"
+    }
   },
   {
     path: '/about',
@@ -18,6 +39,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '*',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container fill-height fluid>
+    <v-row align="center"
+      justify="center">
+      <v-col cols="12">
+        <p class="text-center text-h1">404</p>
+        
+        <p class="text-center text-h4">Not Found</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
